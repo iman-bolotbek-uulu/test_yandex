b0015f5c582c2ec44393ebace46906094b66f180
iman-bolotbek-uulu <iman20030203@outlook.com>
2023-01-22 10:37:00 +0600

    Last commit

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+
+from .models import Author
+from .serializers import AuthorSerializer
+
+
+class AuthorViewSet(viewsets.mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
